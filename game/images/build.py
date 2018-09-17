a0b2e58f583b8ac00141daf53814695e68ab1b98
Danya Generalov <danya02@me.com>
2018-09-17 15:57:54 +0300

    Adding Blender-based image rendering and Minecraft world for BGs.

--- game/images/build.py
+++ game/images/build.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import pygame
 root = os.getcwd()
 opengl_render = True # TODO: get from argv
 for i in os.walk('.'):
@@ -12,8 +13,22 @@ for i in os.walk('.'):
     print('exiting',i[0])
     os.chdir(root)
 
+pygame.init()
+pygame.display.set_mode((10,10))
+
 for i in os.popen('find').read().split('\n'):
     if i.endswith('.png'):
         j=i.split('/')[1:]
-        os.popen('cp "'+i+'" "'+' '.join(j)+'"').read()
+        s = pygame.image.load(i)
+        d = pygame.Surface(s.get_size(),pygame.SRCALPHA, 32)
+        d=d.convert_alpha()
+        for x in range(s.get_width()):
+            for y in range(s.get_height()):
+                c = s.get_at((x,y))
+                if not (c.r==c.b==255 and c.g==0):
+                    d.set_at((x,y),c)
+                else:
+                    d.set_at((x,y),pygame.Color(0,0,0,0))
+        pygame.image.save(d,i)
 
+pygame.quit()
--- game/images/render_opengl.py
+++ game/images/render_opengl.py
@@ -6,10 +6,11 @@ from bpy.app.handlers import persistent
 
 @persistent
 def do_render_opengl(dummy):
+    bpy.data.scenes['Scene'].render.use_antialiasing=False
     bpy.data.scenes['Scene'].render.resolution_percentage=75
     bpy.data.scenes['Scene'].frame_start = 0
     bpy.data.scenes['Scene'].frame_end = bpy.data.scenes['Scene'].frame_start
-    bpy.context.scene.render.alpha_mode = 'TRANSPARENT'
+    bpy.data.scenes['Scene'].render.alpha_mode = 'TRANSPARENT'
     # from https://blender.stackexchange.com/a/17746/61091
     area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
     space = next(space for space in area.spaces if space.type == 'VIEW_3D')
